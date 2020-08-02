886fc7202f4aed40f1bbfa30b057f43acfccd093
Kascique <kascique.lowmans@gmail.com>
2020-08-02 00:24:50 -0400

    Design Update Members Page completed To Do Page added

--- screens/userScreen.js
+++ screens/userScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, View, Text, SafeAreaView, TextBase } from 'react-native';
+import { Title, Avatar } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 import * as firebase from 'firebase';
 
@@ -30,24 +31,44 @@ function EventsScreen(){
 
 function MembersScreen(){
     return(
-        <View>
-            <Text>Members</Text>
+        <View style={global.wrapper}>
+            <View style={styles.wrapper}>
+               <Title>Members</Title>
+               <View style={styles.member}>
+                  <View style={styles.memberCon}>
+                     <Avatar.Image size={70} source={{ uri: 'https://image.freepik.com/free-photo/boy-taking-selfie_23-2148155644.jpg'}}/>
+                     <View style={styles.memberMid}>
+                         <Title style={{fontSize: 20}}>John Doe</Title>
+                         <Text>John.Doe@gmail.com</Text>
+                     </View>
+                     <MaterialCommunityIcons style={styles.memberIcon} name="account-edit" size={40}/>
+                  </View>
+               </View>
+            </View>
         </View>
     )
 }
 
 function ToDoScreen(){
     return(
-        <View>
-            <Text>ToDo</Text>
+        <View style={global.wrapper}>
+            <View style={styles.wrapper}>
+               <Title>To Do</Title>
+               <View style={styles.toDoCon}>
+                   <Title style={styles.toDoTxt}>Wash Dishes</Title>
+                   <MaterialCommunityIcons style={styles.memberIcon} name="check" size={25}/>
+               </View>
+            </View>
         </View>
     )
 }
 
 function JobsScreen(){
     return(
-        <View>
-            <Text>Jobs</Text>
+        <View style={global.wrapper}>
+            <View style={styles.wrapper}>
+               <Title>Jobs</Title>
+            </View>
         </View>
     )
 }
@@ -105,3 +126,48 @@ export default function User({ navigation }){
             </View>
     )
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        width: '90%',
+        alignSelf: 'center'
+    },
+    member: {
+       width: '100%',
+       height: 100,
+       backgroundColor: '#F7F7F7',
+       borderRadius: 20,
+       marginTop: 10,
+       justifyContent: 'center',
+    },
+    memberCon: {
+        width: '96%',
+        alignSelf: 'center',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    memberMid: {
+        flex: 1,
+        marginLeft: 10,
+        height: '100%',
+        justifyContent: 'center',
+    },
+    memberIcon: {
+        color: '#DBDBDB',
+        marginRight: 10
+    },
+
+    toDoCon: {
+        width: '100%',
+        height: 60,
+        backgroundColor: 'red',
+        borderRadius: 10,
+        alignSelf: 'center',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    toDoTxt: {
+        flex: 1,
+        marginLeft: 20
+    }
+})
